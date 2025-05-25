2c46419aa1084788de4250c7651bb25e4f0e7312
John Cook-Martin <johncookmartin@gmail.com>
2025-05-25 17:08:29 -0400

    Adding data access to dependency injection

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -1,3 +1,5 @@
+using FarmaTechData.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddScoped<IProductData, ProductData>();
 
 var app = builder.Build();
 
