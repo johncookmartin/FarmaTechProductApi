1de5f1c5461e031d54e2b5c0ebfa97bec8551484
John Cook-Martin <johncookmartin@gmail.com>
2025-05-27 18:53:53 -0400

    Removing some files that were pushed in error

--- FarmaTechBlob/IBlobService.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace FarmaTechBlob;
-
-public interface IBlobService
-{
-    Task<Stream> StreamAsync(string fileName);
-    Task<(string BlobUrl, string BlobPath)> UploadAsync(string fileName, Stream fileStream);
-}
